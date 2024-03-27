4fcf8c17ae133924fef730e7609b815fa24426ac
Rui Paulo <86334233+RPDJF@users.noreply.github.com>
2024-03-27 15:06:58 +0100

    format minishell header

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rude-jes <rude-jes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 17:02:09 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/03/27 14:44:13 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/03/27 15:06:23 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,19 @@ typedef struct s_minishell
 
 # define VERSION "0.1"
 
-//		init_minishell: init a new minishell instance
-t_minishell	*init_minishell(void);
+// FROM utils/exit_handler.c
+
 //		crash_exit: exit the program when unexpected error
 void		crash_exit(void);
 
+//	FROM utils/init_minishell.c
+
+//		init_minishell: init a new minishell instance
+t_minishell	*init_minishell(void);
+
+// FROM prompt.c
+
+//		prompt: sends a prompt to the user and returns their input as char*
 char		*prompt(t_minishell *minishell);
+
 #endif
