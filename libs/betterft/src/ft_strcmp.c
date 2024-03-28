05b88f3ebe7ef70ab6d320ddfddf2ad5e138a15e
RPDJF <ruipaulo.unify@outlook.fr>
2024-03-28 14:27:18 +0100

    Added ft_strcmp to betterft

--- libs/betterft/src/ft_strcmp.c
+++ libs/betterft/src/ft_strcmp.c
@@ -6,11 +6,10 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 07:02:51 by marvin            #+#    #+#             */
-/*   Updated: 2024/03/28 14:18:34 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/03/28 14:27:01 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 #include "../betterft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
@@ -25,14 +24,4 @@ int	ft_strcmp(const char *s1, const char *s2)
 		i++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-int main(void)
-{
-	ft_printf("%d\n", ft_strcmp("ok", "ok"));
-	ft_printf("%d\n", ft_strcmp("nok", "ok"));
-	ft_printf("%d\n", ft_strcmp("ok", "nok"));
-	ft_printf("%d\n", ft_strcmp("nok", 0));
-	ft_printf("%d\n", ft_strcmp(0, "nok"));
-	ft_printf("%d\n", ft_strcmp(0, 0));
 }
