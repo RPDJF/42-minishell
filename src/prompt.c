de5dacde7675fbf6f69cebed9928c10d0b5b6194
RPDJF <ruipaulo.unify@outlook.fr>
2024-03-29 21:56:05 +0100

    Color support for linux

--- src/prompt.c
+++ src/prompt.c
@@ -6,40 +6,49 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 19:46:49 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/03/29 20:35:57 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/03/29 21:54:30 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
 
+static char	*parse_prompt(char *prompt)
+{
+	char	*home;
+	char	*tmp;
+
+	home = getenv("HOME");
+	if (home)
+	{
+		tmp = prompt;
+		prompt = ft_strreplace(prompt, home, "~");
+		free(tmp);
+	}
+	return (prompt);
+}
+
 static char	*get_prompt(t_minishell *minishell)
 {
 	char	*output;
 	char	*cwd;
-	char	*home;
-	char	*tmp;
 
 	cwd = getcwd(0, 0);
-	home = 0;
+	if (!cwd)
+		crash_exit();
 	if (minishell->hostname && getenv("USER"))
-		output = ft_strsjoin(10, C_MAGENTA, getenv("USER"), "@", \
-						minishell->hostname, C_RESET, ":", C_CYAN, \
-						getcwd(0, 0), C_RESET, "❥ ");
+		output = ft_strsjoin(10, C_MAGENTA, getenv("USER"), "@",
+				minishell->hostname, C_RESET, ":", C_CYAN,
+				cwd, C_RESET, "❥ ");
 	else if (getenv("USER"))
-		output = ft_strsjoin(6, C_MAGENTA, getenv("USER"),
-				":", getcwd(0, 0), C_RESET, "❥ ");
+		output = ft_strsjoin(8, C_MAGENTA, getenv("USER"),
+				C_RESET, ":", C_CYAN, cwd, C_RESET, "❥ ");
 	else
-		output = ft_strsjoin(5, "minishell-", VERSION, ":", cwd, "❥ ");
+		output = ft_strsjoin(8, C_MAGENTA, "minishell-", C_CYAN, VERSION,
+				C_RESET, ":", cwd, "❥ ");
 	if (!output)
 		crash_exit();
-	home = getenv("HOME");
-	if (home)
-	{
-		tmp = output;
-		output = ft_strreplace(output, home, "~");
-		free(tmp);
-	}
 	free(cwd);
+	output = parse_prompt(output);
 	return (output);
 }
 
