9d75cd86c8285736770b7b62509c944d12b77b92
RPDJF <ruipaulo.unify@outlook.fr>
2024-03-30 19:31:03 +0100

    some work

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 23:10:40 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/03/30 19:14:46 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/03/30 19:30:50 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	execute_cmd(t_minishell *minishell, t_cmd *cmd)
 	if (cmd->pid == 0)
 	{
 		execve(cmd->cmd, cmd->argv, minishell->envp);
-		crash_exit();
+		error_exit(0, "command not found", 127);
 	}
 	else if (cmd->pid < 0)
 		crash_exit();
