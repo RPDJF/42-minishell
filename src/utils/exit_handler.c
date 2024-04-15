fd7ed6faebc13fbfa69899fc1b52046b1fe03218
RPDJF <ruipaulo.unify@outlook.fr>
2024-04-15 22:37:32 +0200

    fix crash_exit

--- src/utils/exit_handler.c
+++ src/utils/exit_handler.c
@@ -1,14 +1,9 @@
 #include "exit_handler.h"
-#include "../minishell.h"
 
 void	crash_exit(void)
 {
-	int	errno_tmp;
-
-	errno_tmp = errno;
-	ft_putstr_fd("minishell: ", STDERR_FILENO);
-	ft_putendl_fd(strerror(errno), STDERR_FILENO);
-	exit(errno_tmp);
+	perror(APP_NAME);
+	exit(errno);
 }
 
 void	error_msg(char **context, char *msg)
