c6b241e983e3a0994dba889216a5bf92d4493412
RPDJF <ruipaulo.unify@outlook.fr>
2024-04-17 23:53:31 +0200

    fix var_to_tab exporting local variables

--- src/env/env_conv.c
+++ src/env/env_conv.c
@@ -2,23 +2,25 @@
 
 char	**var_to_tab(void)
 {
-	t_minishell	*minishell;
 	t_var		*var;
 	char		**tab;
 	int			i;
 
-	minishell = get_minishell();
-	var = minishell->mini_envp;
+	var = get_minishell()->mini_envp;
 	tab = 0;
 	i = 0;
 	while (var)
 	{
-		tab = ft_reallocf(tab, i * sizeof(char *), (i + 1) * sizeof(char *));
-		if (!tab)
-			crash_exit();
-		tab[i] = ft_strsjoin(3, var->name, "=", var->value);
+		if (var->is_env)
+		{
+			tab = ft_reallocf(tab, i * sizeof(char *),
+					(i + 1) * sizeof(char *));
+			if (!tab)
+				crash_exit();
+			tab[i] = ft_strsjoin(3, var->name, "=", var->value);
+			i++;
+		}
 		var = var->next;
-		i++;
 	}
 	tab = ft_reallocf(tab, i * sizeof(char *), (i + 1) * sizeof(char *));
 	if (!tab)
