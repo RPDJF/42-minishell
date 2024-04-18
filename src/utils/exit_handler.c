23a0b6e3bc2456749c2423bd8ff1f85375f062a0
RPDJF <ruipaulo.unify@outlook.fr>
2024-04-18 17:06:27 +0200

    update err message display

--- src/utils/exit_handler.c
+++ src/utils/exit_handler.c
@@ -12,21 +12,15 @@ void	crash_exit(void)
 
 void	error_msg(char **context, char *msg)
 {
-	bool	is_first;
-
-	is_first = true;
 	while (context && *context)
 	{
-		if (is_first)
-			ft_putstr_fd(C_RED, STDERR_FILENO);
 		ft_putstr_fd(*context, STDERR_FILENO);
 		ft_putstr_fd(": ", STDERR_FILENO);
-		if (is_first)
-			ft_putstr_fd(C_RESET, STDERR_FILENO);
-		is_first = false;
 		context++;
 	}
+	ft_putstr_fd(C_RED, STDERR_FILENO);
 	ft_putendl_fd(msg, STDERR_FILENO);
+	ft_putstr_fd(C_RESET, STDERR_FILENO);
 }
 
 void	error_exit(char **context, char *msg, int exitcode)
