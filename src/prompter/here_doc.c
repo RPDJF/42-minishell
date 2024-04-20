0f741b0f14f3c8c3409c29db6b00286474e7d54d
RPDJF <ruipaulo.unify@outlook.fr>
2024-04-20 17:54:17 +0200

    Refactor prompt_here_doc function to handle end-of-file delimiter properly

--- src/prompter/here_doc.c
+++ src/prompter/here_doc.c
@@ -7,10 +7,26 @@ static int	handle_sigint(int *pipe_fd)
 	return (-1);
 }
 
-char	*prompt_here_doc(void)
+char	*prompt_here_doc(char *delimiter)
 {
+	char	*line;
+	char	*msg;
+
 	write (STDOUT_FILENO, "> ", 2);
-	return (ft_get_next_line(STDIN_FILENO));
+	line = ft_get_next_line(STDIN_FILENO);
+	if (!line)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		msg = ft_strsjoin(3, "here-document delimited by end-of-file (wanted `",
+				delimiter, "')");
+		if (!msg)
+			crash_exit();
+		error_msg((char *[]){APP_NAME, "warning", 0}, msg);
+		gfree(msg);
+		return (0);
+	}
+	line[ft_strlen(line) - 1] = 0;
+	return (line);
 }
 
 int	here_doc(char *delimiter)
@@ -22,14 +38,14 @@ int	here_doc(char *delimiter)
 		crash_exit();
 	get_minishell()->here_doc_fd[0] = pipe_fd[0];
 	get_minishell()->here_doc_fd[1] = pipe_fd[1];
-	line = prompt_here_doc();
+	line = prompt_here_doc(delimiter);
 	while (line && ft_strcmp(line, delimiter))
 	{
 		ft_putendl_fd(line, pipe_fd[1]);
 		free(line);
 		if (get_minishell()->sigint == SIGINT)
 			return (handle_sigint(pipe_fd));
-		line = prompt_here_doc();
+		line = prompt_here_doc(delimiter);
 	}
 	close(pipe_fd[1]);
 	get_minishell()->here_doc_fd[0] = 0;
