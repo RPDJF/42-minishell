8ac85244ff74e8a6fe67c038a7a8ffb14b1f341e
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-04-21 20:38:12 +0200

    update for rui get the is_var_init

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,12 +1,25 @@
 #include "../lexer/lexer.h"
 #include "parsing.h"
 
+char	*tw_strchr(t_word *cmd, int c)
+{
+	t_word	*tmp;
+
+	while (tmp)
+	{
+		if (ft_strchr(tmp->str, c) != NULL)
+			return (ft_strchr(tmp->str, c));
+		else
+			tmp = tmp->next;
+	}
+	return (NULL);
+}
+
 t_token	*parsing(t_tlex *lex)
 {
 	t_token	*token;
+	t_tlex	*tmp1;
+	t_tlex	*tmp2;
 
-	token = NULL;
-	(void)lex;
-	(void)token;
 	return (token);
 }
