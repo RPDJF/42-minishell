e076cacab36a4919699395f2505717483744644b
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-04-23 04:36:22 +0200

    update

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -37,7 +37,7 @@ int	var_quoted(t_word *cmd)
 	return (0);
 }
 
-int	do_tk_is_var_init(t_token *new, t_pars *pars)
+int	tk_is_var_init(t_token *new, t_pars *pars)
 {
 	(void)new;
 	(void)pars;
@@ -90,7 +90,7 @@ int	arg_count(t_tlex *arg)
 	while (tmp)
 	{
 		if (tw_is_delem(tmp->cmd) == 1)
-			return (i);
+			break ;
 		else if (tw_is_delem(tmp->cmd) == 2)
 		{
 			if (tmp->next)
@@ -103,6 +103,70 @@ int	arg_count(t_tlex *arg)
 			i++;
 		tmp = tmp->next;
 	}
+	return (i);
+}
+
+t_token_type	define_type(t_word *cmd)
+{
+	if (cmd->is_quoted == 0 && !cmd->next)
+	{
+		if (cmd->str[0] == '<' && !cmd->str[1])
+			return (token_stdin);
+		if (cmd->str[0] == '<' && cmd->str[1] == '<' && !cmd->str[2])
+			return (token_stdin);
+		if (cmd->str[0] == '>' && !cmd->str[1])
+			return (token_stdout);
+		if (cmd->str[0] == '>' && cmd->str[1] == '>' && !cmd->str[2])
+			return (token_stdout);
+	}
+	return (0);
+}
+
+char	*join_tword(t_word	*cmd)
+{
+	char	*filename;
+	char	*tmp1;
+
+	filename = cmd->str;
+	while ()
+	;
+}
+
+t_token	*newtk_delem(t_word *cmd, t_pars *pars)
+{
+	t_token	*new;
+
+	new = ft_calloc(1, sizeof(t_token));
+	if (!new)
+		crash_exit();
+	new->type = define_type(cmd);
+	if (new->type == token_stdin && cmd->str[0] == '<' && !cmd->str[1])
+	{
+		new->data = (t_stdin *)ft_calloc(1, sizeof(t_stdin));
+		((t_stdin *)(new)->data)->filename = join_tword(pars->tmp1->next->cmd);
+		////////////////////////////////////////////////////////////////////////////
+	}
+}
+
+int	pars_arg_tk_cmd(t_token *new, t_cmd *token, t_pars *pars)
+{
+	int	i;
+
+	i = 1;
+	while (pars->tmp1)
+	{
+		if (tw_is_delem(pars->tmp1->cmd) == 1)
+			break ;
+		else if (tw_is_delem(pars->tmp1->cmd) == 2)
+			token_add_back(&new, newtk_delem(pars->tmp1->cmd, pars));
+		else
+		{
+			token->argv[i] = pars->tmp1->cmd;
+			i++;
+		}
+		pars->tmp1 = pars->tmp1->next;
+	}
+	token->argc = i;
 	return (i);
 }
 
@@ -119,10 +183,13 @@ int	tk_cmd(t_token *new, t_pars *pars)
 	if (!data_tmp->argv)
 		crash_exit();
 	data_tmp->argv[0] = pars->tmp1->cmd;
+	pars->tmp1 = pars->tmp1->next;
+	if (!pars_arg_tk_cmd(new, data_tmp, pars))
+		return (0);
 	return (1);
 }
 
-t_token	*token_new(t_tlex *lex, t_pars *pars)
+t_token	*token_new(t_pars *pars)
 {
 	t_token	*new;
 
@@ -130,7 +197,7 @@ t_token	*token_new(t_tlex *lex, t_pars *pars)
 	if (!new)
 		crash_exit();
 	if ((tw_strchr(pars->tmp1->cmd, '=') && var_quoted(pars->tmp1->cmd)
-			&& !do_tk_is_var_init(new, pars)))
+			&& !tk_is_var_init(new, pars)))
 		return (NULL);
 	else if (!tk_cmd(new, pars))
 		return (NULL);
@@ -147,7 +214,7 @@ t_token	*parsing(t_tlex **lex)
 	pars.tmp1 = *lex;
 	while (pars.tmp1)
 	{
-		new = token_new(*lex, &pars);
+		new = token_new(&pars);
 		if (!new)
 			return (free_exit_token(&token, lex));
 		token_add_back(&token, new);
