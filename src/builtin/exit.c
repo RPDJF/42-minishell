fc04e33211b88506a206b29202bc55fca981d044
RPDJF <ruipaulo.unify@outlook.fr>
2024-04-23 13:07:57 +0200

    fix exit builtin

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -10,13 +10,27 @@ static void	fn(char **context, char *errmsg, int exitcode)
 
 int	exit_ms(int argc, char **argv)
 {
-	if (argv[1] && ft_strcmp(ft_itoa(ft_atoi(argv[1])), argv[1]))
+	char	*cmp;
+	int		exitcode;
+
+	if (argc <= 2 && argv[1])
+	{
+		exitcode = ft_atoi(argv[1]);
+		cmp = ft_itoa(exitcode);
+		if (!cmp)
+			crash_exit();
+		if (argv[1][0] == '+')
+			argv[1]++;
+		if (!ft_strcmp(argv[1], cmp))
+			fn(0, 0, exitcode);
 		fn((char *[]){APP_NAME, "exit", argv[1], 0},
 			"numeric argument required", 2);
+	}
 	if (argc > 2)
-		fn((char *[]){APP_NAME, "exit", 0}, "too many arguments", 1);
-	if (argv[1])
-		fn(0, 0, ft_atoi(argv[1]));
+	{
+		error_msg((char *[]){APP_NAME, "exit", 0}, "too many arguments");
+		return (1);
+	}
 	else
 		fn(0, 0, 0);
 	return (0);
