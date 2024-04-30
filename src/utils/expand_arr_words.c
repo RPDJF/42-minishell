9be63398f23bde38c878e544c3149f7c0bd08073
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-04-30 23:10:04 +0200

    nothing important

--- src/utils/expand_arr_words.c
+++ src/utils/expand_arr_words.c
@@ -1,8 +1,16 @@
 #include "expand_words.h"
 
+char	**pars_wildcard(char *str)
+{
+	char	**arg;
+
+	arg = 0; 
+}
+
 char	**parse_words_arr(t_word **words)
 {
 	char	**arr;
+	char	**wld;
 	size_t	count;
 	size_t	i;
 
@@ -12,7 +20,11 @@ char	**parse_words_arr(t_word **words)
 		crash_exit();
 	i = -1;
 	while (i++, words[i])
+	{
 		arr[i] = parse_words(words[i]);
+		if (ft_strchr(arr[i], '*'))
+			wld = pars_wildcard(arr[i]);
+	}
 	arr[i] = 0;
 	return (arr);
 }
