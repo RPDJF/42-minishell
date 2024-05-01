efa30d10bdd961e7066c06ee00d0e4779d397d3a
RPDJF <ruipaulo.unify@outlook.fr>
2024-05-01 18:21:25 +0200

    Fix unset function to only remove variables that start with alphabetic characters

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -4,6 +4,7 @@ int	unset(int argc, char **argv)
 {
 	(void)argc;
 	while (++argv, *argv)
-		remove_var(get_var(*argv));
+		if (**argv && ft_isalpha(**argv))
+			remove_var(get_var(*argv));
 	return (0);
 }
