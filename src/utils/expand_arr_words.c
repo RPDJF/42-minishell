3bbdd7cbe9836a475a526dca5219631594a38182
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-05-02 15:48:53 +0200

    add the wildcard fonctions

--- src/utils/expand_arr_words.c
+++ src/utils/expand_arr_words.c
@@ -29,23 +29,68 @@ static char	**get_files(void)
 	return (files);
 }
 
+int	to_next_wildcard(char *arg, char *str, int *i, int *j)
+{
+	if (str[*j] == '*')
+	{
+		while (str[*j] == '*')
+			*j += 1;
+		while (arg[*i] && arg[*i] != str[*j])
+			*i += 1;
+	}
+	else if (*i > 0)
+		while (arg[*i] && arg[*i] != str[*j])
+			*i += 1;
+	if (arg[*i] != str[*j] || !arg[*i])
+		return (0);
+	while (arg[*i] && str[*j] && arg[*i] == str[*j])
+	{
+		*i += 1;
+		*j += 1;
+		if (str[*j] == '*')
+			return (1);
+	}
+	return (1);
+}
+
+int	is_last_wildcard(char *str)
+{
+	if (*str == '*')
+		str++;
+	while (*str)
+	{
+		if (*str == '*')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
 int	is_wildcard(char *arg, char *str)
 {
 	int	i;
 	int	j;
 
-	i = -1;
-	j = -1;
-	while (arg[++i] == str[++j])
-		;
-	if (str[j] == '*')
+	i = 0;
+	j = 0;
+	while (str[j])
 	{
-		j = ft_strlen(str) + 1;
-		i = ft_strlen(arg) + 1;
-		while (i >= 0 && arg[--i] == str[--j])
-			;
-		if (str[j] == '*')
-			return (1);
+		if (!is_last_wildcard(str + j))
+		{
+			if (!to_next_wildcard(arg, str, &i, &j))
+				return (0);
+		}
+		else if (is_last_wildcard(str + j))
+		{
+			j = ft_strlen(str) + 1;
+			i = ft_strlen(arg) + 1;
+			while (i >= 0 && arg[--i] == str[--j])
+				;
+			if (str[j] == '*')
+				return (1);
+			else
+				return (0);
+		}
 	}
 	return (0);
 }
@@ -61,8 +106,12 @@ char	**pars_wildcard(char *str)
 	arg = get_files();
 	while (arg[i])
 	{
-		if (is_wildcard(arg[i], str) && arg[i][0] != '.')
+		if (is_wildcard(arg[i], str))
+		{
+			if (str[0] == '*' && !str[1] && arg[i][0] == '.' && i++)
+				continue ;
 			tmp = strr_realloc(tmp, arg[i]);
+		}
 		i++;
 	}
 	ft_free_tab(arg);
@@ -84,34 +133,49 @@ void	realloc_arr(char ***arr, char **wld, size_t *i)
 		gfree(wld[j]);
 		j++;
 	}
-	while (arr[0][*i])
+	while (arr[0][*i + 1])
 		*i += 1;
+}
+
+char	**wildcard_quoted(t_word *cmd)
+{
+	char	**is_quoted;
+
+	is_quoted = NULL;
+	while (cmd)
+	{
+		if (cmd->is_quoted == true)
+			is_quoted = strr_realloc(is_quoted, "1");
+		if (cmd->is_quoted == false)
+			is_quoted = strr_realloc(is_quoted, "0");
+		cmd = cmd->next;
+	}
+	return (is_quoted);
 }
 
 char	**parse_words_arr(t_word **words)
 {
 	char	**arr;
 	char	**wld;
-	size_t	count;
+	char	**is_quoted;
+	size_t	j;
 	size_t	i;
 
-	count = words_arr_len(words);
-	arr = galloc(sizeof(char *) * (count + 1));
-	if (!arr)
-		crash_exit();
-	count = 0;
+	arr = NULL;
+	j = -1;
 	i = -1;
-	while (i++, words[count])
+	while (i++, words[++j])
 	{
-		arr[i] = parse_words(words[count]);
+		is_quoted = wildcard_quoted(words[j]);
+		arr = strr_realloc(arr, parse_words(words[j]));
 		if (ft_strchr(arr[i], '*'))
 		{
 			wld = pars_wildcard(arr[i]);
 			if (wld)
 				realloc_arr(&arr, wld, &i);
 		}
-		count++;
+		if (is_quoted && *is_quoted)
+			ft_free_tab(is_quoted);
 	}
-	arr[i] = 0;
 	return (arr);
 }
