0fc6c24b11c2e6296e2525040999b5708215e3b5
RPDJF <ruipaulo.unify@outlook.fr>
2024-05-04 03:32:28 +0200

    Fix memory allocation issue in ft_strjoin.c

--- libs/betterft/src/ft_strjoin.c
+++ libs/betterft/src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 10:04:30 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/01/23 02:31:39 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/05/03 21:49:58 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	else if (s1 && !s2)
+		return (ft_strdup(s1));
+	else if (!s1 || !s2)
+		return (0);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	s3 = (char *)galloc((s1_len + s2_len + 1) * sizeof(char));
 	if (!s3)
-		return (NULL);
+		return (0);
 	ft_memcpy(s3, s1, s1_len);
 	ft_memcpy((s3 + s1_len), s2, s2_len);
 	s3[s1_len + s2_len] = 0;
