eede7eff6f4d262ac1963fe48ef95dd273b7679a
RPDJF <ruipaulo.unify@outlook.fr>
2024-05-06 15:42:12 +0200

    Fix ft_strreplace_first function to correctly replace the first occurrence of a substring

--- libs/betterft/src/ft_strreplace_first.c
+++ libs/betterft/src/ft_strreplace_first.c
@@ -6,7 +6,7 @@
 /*   By: rude-jes <rude-jes@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 13:19:49 by rude-jes          #+#    #+#             */
-/*   Updated: 2024/04/26 00:25:35 by rude-jes         ###   ########.fr       */
+/*   Updated: 2024/05/06 15:41:44 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 static void	fill_str(char *src, char *old, char *new, char *output)
 {
-	char	*tmp;
+	char	*needle;
 	size_t	size;
 
-	size = 0;
-	tmp = ft_strnstr(src, old, ft_strlen(src));
-	ft_strlcpy(output + size, src, tmp - src + 1);
-	size += tmp - src;
-	src += tmp - src;
-	ft_strlcpy(output + size, new, ft_strlen(new) + 1);
-	size += ft_strlen(new);
-	src += ft_strlen(old);
-	ft_strlcpy(output + size, src, tmp - src + 1);
+	size = ft_strlen(src) - ft_strlen(old) + ft_strlen(new) + 1;
+	needle = ft_strnstr(src, old, ft_strlen(src));
+	ft_strlcpy(output, src, needle - src + 1);
+	ft_strlcat(output, new, size);
+	ft_strlcat(output, needle + ft_strlen(old), size);
 }
 
 char	*ft_strreplace_first(char *src, char *old, char *new)
